512184429fe53549ef0027950495ae3e6ab5af74
Jorgel48 <60270909+Jorgel48@users.noreply.github.com>
2020-12-05 14:13:23 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Rectangle_Class10._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectangleTest rect = new RectangleTest(11.5f, 15.4f);
+            Console.WriteLine(rect);
+
+            Console.ReadLine();
+        }
+    }   
+
+
+}
---
+++ RectangleTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+
+namespace Rectangle_Class10._3
+{
+    class RectangleTest
+    {
+        private float length = 1;
+        private float width = 1;
+
+        public RectangleTest(float length, float width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public float Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value <= 0.0f || value > 20.0f)
+                    this.length = 1.0f;
+                else
+                    this.length = value;
+            }
+        }
+
+        public float Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value <= 0.0f || value > 20.0f)
+                    this.width = 1.0f;
+                else
+                    this.width = value;
+            }
+        }
+
+        public float Perimeter
+        {
+            get
+            {
+                return ((Length + Width) * 2);
+            }
+        }
+       
+        public float Area
+        {
+            get
+            {
+                return (Length * Width); 
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Perimeter is {0:F2} and the area is {1:F2}", Perimeter, Area);
+        }
+    }
+
+}
+
